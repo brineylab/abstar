66f3c533a93a0289ed4010d12dd1836c7e223599
bryanbriney <bryanbriney@Bryans-MacBook-Pro.local>
2016-02-25 23:51:38 -0800

    add abtools to setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ config = {
 	# 'download_url': 'www.github.com/briney/abstar/',
 	'author_email': 'briney@scripps.edu',
 	'version': '0.1.0',
-	'install_requires': [
-						 # 'abtools',
+	'install_requires': ['abtools',
 						 'biopython',
 						 'celery',
 						 'nwalign',
