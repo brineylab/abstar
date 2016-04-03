fbf4109eed78ec5923d7f58ac9222adc53b43ddc
bryanbriney <bryanbriney@Bryans-MacBook-Pro.local>
2016-04-02 23:14:12 -0700

    switch isotype to an object

--- abstar/utils/isotype.py
+++ abstar/utils/isotype.py
@@ -33,6 +33,7 @@ from Bio.Alphabet import generic_dna
 from abtools import log
 from abtools.alignment import local_alignment
 from abtools.sequence import Sequence
+from abtools.utils.decorators import lazy_property
 
 
 def get_isotype(vdj):
@@ -40,13 +41,42 @@ def get_isotype(vdj):
     try:
         mod_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         isotype_file = os.path.join(mod_dir, 'ssw/isotypes/{}_isotypes.fasta'.format(vdj.species))
-
         isotype_seqs = [Sequence(s) for s in SeqIO.parse(open(isotype_file, 'r'), 'fasta')]
         isotype_seqs += [Sequence((s.id, s.reverse_complement)) for s in isotype_seqs]
-        alignments = local_alignment(vdj.raw_query, targets=isotype_seqs,
-                                     gap_open_penalty=22, gap_extend_penalty=1)
-        alignments.sort(key=lambda x: x.score, reverse=True)
-        return(alignments[0].target.id)
+        return Isotype(vdj, isotype_seqs)
     except:
         logger.debug('ISOTYPE ERROR: {}\t{}'.format(vdj.id, vdj.raw_query))
         logger.debug(traceback.format_exc())
+
+
+
+class Isotype(object):
+    """docstring for Isotype"""
+    def __init__(self, vdj, isotype_seqs):
+        super(Isotype, self).__init__()
+        self._alignments = self._get_alignments(vdj, isotype_seqs)
+        self.alignment = self._alignments[0]
+
+
+    @lazy_property
+    def isotype(self):
+    	if self.alignment.score < 30:
+    		return 'unknown'
+        return self.alignment.target.id
+
+
+    @lazy_property
+    def score(self):
+        return self.alignment.score
+
+
+    def _get_alignments(self, vdj, isotype_seqs):
+        query_region = self._get_isotype_query_region(vdj)
+        alignments = local_alignment(query_region, targets=isotype_seqs,
+            gap_open_penalty=22, gap_extend_penalty=1)
+        return sorted(alignments, key=lambda x: x.score, reverse=True)
+
+
+    def _get_isotype_query_region(self, vdj):
+        aln = local_alignment(vdj.vdj_nt, vdj.raw_input)
+        return vdj.raw_input[aln.target_end:]
