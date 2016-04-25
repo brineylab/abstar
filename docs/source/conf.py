b007ade9cbbd46b3af2eb949e19cab7ac40445ed
briney <bryanbriney@gmail.com>
2016-04-25 00:42:56 -0700

    Update docs conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ class Mock(MagicMock):
             return Mock()
 
 MOCK_MODULES = ['pygtk', 'gtk', 'gobject', 'argparse', 'numpy', 'nwalign', 'pandas',
-                'abtools', 'biopython', 'celery', 'pymongo', 'scikit-bio',
+                'abtools', 'abtools.sequence', 'biopython', 'celery', 'pymongo', 'scikit-bio',
                 'Bio', 'Bio.Align', 'Bio.Alphabet', 'Bio.SeqIO', 'Bio.Seq', 'Bio.SeqRecord']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
