77057482afdbf23cc3c043b222eae2ab45f73c89
bryanbriney <bryanbriney@Bryans-MacBook-Pro.local>
2016-06-15 10:46:43 -0700

    bump version to 0.1.6

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ config = {
     'url': 'www.github.com/briney/abstar/',
     # 'download_url': 'www.github.com/briney/abstar/',
     'author_email': 'briney@scripps.edu',
-    'version': '0.1.5',
+    'version': '0.1.6',
     'install_requires': install_requires,
     'packages': ['abstar'],
     'scripts': ['bin/abstar',
