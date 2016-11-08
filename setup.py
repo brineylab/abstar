0476accbea306d4127ad6ca51a34511d52814d99
bryanbriney <bryanbriney@dhcp-228-113.scripps.edu>
2016-11-08 15:01:11 -0800

    bump version to 0.1.10

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ config = {
     'url': 'https://www.github.com/briney/abstar',
     # 'download_url': 'www.github.com/briney/abstar/',
     'author_email': 'briney@scripps.edu',
-    'version': '0.1.9',
+    'version': '0.1.10',
     'install_requires': install_requires,
     'packages': ['abstar'],
     'scripts': ['bin/abstar',
