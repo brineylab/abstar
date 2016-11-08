c0b33762716d5afe0a37bf33c45c564c7086a03f
bryanbriney <bryanbriney@dhcp-228-113.scripps.edu>
2016-11-08 14:55:16 -0800

    fixed logging for Celery tasks

--- abstar/utils/vdj.py
+++ abstar/utils/vdj.py
@@ -336,11 +336,11 @@ def run(seq_file, output_dir, arg_dict):
     try:
         from abstar.abstar import Args
         args = Args(**arg_dict)
-        global logger
-        if args.cluster:
-            logger = get_task_logger(__name__)
-        else:
-            logger = log.get_logger(__name__)
+        # global logger
+        # if args.cluster:
+        #     logger = get_task_logger(__name__)
+        # else:
+        #     logger = log.get_logger(__name__)
         output_filename = os.path.basename(seq_file)
         if args.output_type == 'json':
             output_file = os.path.join(output_dir, output_filename + '.json')
@@ -353,7 +353,7 @@ def run(seq_file, output_dir, arg_dict):
         output_count = write_output(clean_vdjs, output_file, args.output_type, args.pretty, args.padding)
         return (output_file, output_count)
     except:
-        logger.debug(traceback.format_exc())
+        # logger.debug(traceback.format_exc())
         raise Exception("".join(traceback.format_exception(*sys.exc_info())))
         # run.retry(exc=exc, countdown=5)
 
@@ -370,8 +370,6 @@ def write_output(output, outfile, output_type, pretty, padding):
 
 
 def process_sequences(sequences, args):
-    global logger
-    logger = logging.getLogger()
     seq_file = tempfile.NamedTemporaryFile(delete=False)
     seq_file.write('\n'.join([s.fasta for s in sequences]))
     seq_file.close()
@@ -388,6 +386,9 @@ def process_sequence_file(seq_file, args):
 
     Output is a list of VDJ objects.
     '''
+
+    global logger
+    logger = logging.getLogger()
 
     # Variable gene assignment
     vs = []
