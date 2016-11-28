bcfbeef49ea85cadff05a0fc686b97b182bcd1a0
bryanbriney <bryanbriney@dhcp-228-113.scripps.edu>
2016-11-28 15:38:07 -0800

    clean up pandaseq logging to screen

--- abstar/utils/pandaseq.py
+++ abstar/utils/pandaseq.py
@@ -178,7 +178,7 @@ def run(input, output, algorithm='simple_bayesian', nextseq=False):
     merged_files = []
     for i, pair in enumerate(sorted(pairs.keys())):
         if len(pairs[pair]) == 2:
-            logger.info('[ {} ]  Merging {} and {}'.format(i + 1, pairs[pair][0], pairs[pair][1]))
+            # logger.info('[ {} ]  Merging {} and {}'.format(i + 1, pairs[pair][0], pairs[pair][1]))
             mf = merge_reads(pairs[pair], output, algorithm, nextseq, i)
             merged_files.append(mf)
     return merged_files
