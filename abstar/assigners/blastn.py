9935c2be8e16766bf48f6c3c74d61c7ae4be89a8
bryanbriney <bryanbriney@Bryans-MacBook-Pro.local>
2016-12-30 23:40:10 -0800

    fix typo in J-GENE QUERY ERROR logging

--- abstar/assigners/blastn.py
+++ abstar/assigners/blastn.py
@@ -91,7 +91,7 @@ class Blastn(BaseAssigner):
                     vdj.log('J-GENE QUERY ERROR:', 'Query sequence for J-gene assignment is too short.')
                     vdj.log('')
                     query = vbr.alignments[0].hsps[0].query
-                    subject = vbr.alignments[0].hsps[0].subject
+                    subject = vbr.alignments[0].hsps[0].sbjct
                     vdj.log(' QUERY :', query)
                     vdj.log(''.join(['|' if _q == _s else ' ' for _q, _s in zip(q, s)]))
                     vdj.log('SUBJECT:', s)
