205cb379e6b92caf7a9c0e2dc206c1ad9198c5da
bryanbriney <bryanbriney@wireless-189-113.scripps.edu>
2017-10-25 13:12:49 -0700

    fix removal of empty key/value pairs in JSON output

--- abstar/utils/output.py
+++ abstar/utils/output.py
@@ -328,7 +328,7 @@ class AbstarResult(object):
             output['padding'] = ['n' * 100] * 10
 
         # remove empty entries to save MongoDB space
-        for i in output.keys():
+        for i in list(output.keys()):
             if output[i] == "":
                 del output[i]
             elif output[i] == []:
