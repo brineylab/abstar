2f4c3f18ff0de9364120b0fd5a799bc50cad83bb
briney <bryanbriney@gmail.com>
2018-04-25 17:58:53 -0700

    bump PyPI version to 0.3.3

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ config = {
     'url': 'https://www.github.com/briney/abstar',
     # 'download_url': 'www.github.com/briney/abstar/',
     'author_email': 'briney@scripps.edu',
-    'version': '0.3.2',
+    'version': '0.3.3',
     'install_requires': install_requires,
     'packages': ['abstar'],
     'scripts': ['bin/abstar',
