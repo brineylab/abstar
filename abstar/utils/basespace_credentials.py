1214d2087f9be249b05bdb7d7e22bb6b5a15cb35
briney <bryanbriney@gmail.com>
2018-04-25 17:57:50 -0700

    add secondary config file creation

--- abstar/utils/basespace_credentials.py
+++ abstar/utils/basespace_credentials.py
@@ -56,17 +56,27 @@ def main():
     creds['version'] = raw_input('Version [v1pre3]: ')
     if not creds['version'].strip():
         creds['version'] = 'v1pre3'
-    write_cred_file(creds)
+    write_cred_files(creds)
 
 
-def write_cred_file(creds):
+def write_cred_files(creds):
+    # credentials file
     jcreds = json.dumps(creds)
     cred_file = os.path.expanduser('~/.abstar/basespace_credentials')
     if not os.path.exists(os.path.dirname(cred_file)):
         os.makedirs(os.path.dirname(cred_file))
-    cred_handle = open(cred_file, 'w')
-    cred_handle.write(jcreds)
-    print('\nCredentials have been saved to {}\n'.format(cred_file))
+    with open(cred_file, 'w') as cred_handle:
+        cred_handle.write(jcreds)
+    print('\nCredentials have been saved to {}'.format(cred_file))
+    # config file
+    cfg_file = os.path.expanduser('~/.basespace/default.cfg')
+    if not os.path.exists(os.path.dirname(cfg_file)):
+        os.makedirs(os.path.dirname(cfg_file))
+    cfg_string = '[DEFAULT]\naccessToken = {}\napiServer = {}\n'.format(creds['access_token'],
+                                                                        creds['api_server'])
+    with open(cfg_file, 'w') as cfg_handle:
+        cfg_handle.write(cfg_string)
+    print('Configuration info has been saved to {}\n'.format(cfg_file))
 
 
 if __name__ == '__main__':
