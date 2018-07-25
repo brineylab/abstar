9f86b1c2e98a226f63213168ac7918de52b50e9a
briney <bryanbriney@gmail.com>
2018-07-25 13:31:13 -0700

    switch back to universal reference antibody pickles

--- abstar/tests/__init__.py
+++ abstar/tests/__init__.py
@@ -9,8 +9,10 @@ if sys.version_info[0] > 2:
     bnab_lc_antibodies_file = 'bnab_lc_antibodies.pkl'
 else:
     import cPickle as pickle
-    bnab_hc_antibodies_file = 'bnab_hc_antibodies_py2.pkl'
-    bnab_lc_antibodies_file = 'bnab_lc_antibodies_py2.pkl'
+    bnab_hc_antibodies_file = 'bnab_hc_antibodies.pkl'
+    bnab_lc_antibodies_file = 'bnab_lc_antibodies.pkl'
+    # bnab_hc_antibodies_file = 'bnab_hc_antibodies_py2.pkl'
+    # bnab_lc_antibodies_file = 'bnab_lc_antibodies_py2.pkl'
 
 with open(os.path.join(data_dir, bnab_hc_antibodies_file), 'rb') as f:
     REFERENCE_BNAB_HC_ANTIBODIES = pickle.load(f)
