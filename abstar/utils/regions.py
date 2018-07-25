e68c46278138dac71eced8e609d388b3d831e85d
briney <bryanbriney@gmail.com>
2018-07-25 15:30:49 -0700

    fix translation warning for rare sequences of non-codon length

--- abstar/utils/regions.py
+++ abstar/utils/regions.py
@@ -188,6 +188,8 @@ class BaseRegions(object):
                     to_translate[region] = region_nt
             # translate the sequences for each region
             for region, nt_seq in to_translate.items():
+                if len(nt_seq) % 3:
+                    nt_seq = nt_seq[:-(len(nt_seq) % 3)]
                 aa_seq = str(Seq(nt_seq, generic_dna).translate())
                 aa_seqs[region] = aa_seq
             self._aa_seqs = aa_seqs
