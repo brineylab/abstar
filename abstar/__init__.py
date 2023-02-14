d738f3efddece6e523322c8ed93d0b3fea9180fb
Bryan Briney <bryanbriney@gmail.com>
2023-02-14 14:16:22 -0800

    new macaque germline database

--- abstar/__init__.py
+++ abstar/__init__.py
@@ -1,6 +1,7 @@
 import warnings
 from Bio import BiopythonWarning
-warnings.simplefilter('ignore', BiopythonWarning)
+
+warnings.simplefilter("ignore", BiopythonWarning)
 
 from .core.abstar import run, run_standalone, main, parse_arguments, validate_args
 from .preprocess import fastqc, adapter_trim, quality_trim
