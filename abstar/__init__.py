940736489620b2d5d8dc77c8884e87abab0dd593
Bryan Briney <bryanbriney@gmail.com>
2023-03-02 08:58:07 -0800

    fix preprocess imports

--- abstar/__init__.py
+++ abstar/__init__.py
@@ -4,6 +4,7 @@ from Bio import BiopythonWarning
 warnings.simplefilter("ignore", BiopythonWarning)
 
 from .core.abstar import run, run_standalone, main, parse_arguments, validate_args
-from .preprocess import fastqc, adapter_trim, quality_trim
+
+# from .preprocess import fastqc, adapter_trim, quality_trim
 
 from .version import __version__
