5eaadea3f34ac7d3d93e9f75721f23b88da722db
Bryan Briney <bryanbriney@gmail.com>
2024-03-11 15:23:04 -0700

    fix cigar tests

--- abstar/tests/test_cigar.py
+++ abstar/tests/test_cigar.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ..utils.cigar import make_cigar, make_alignment_cigar
+from ..utils.cigar import make_alignment_cigar, make_cigar
 
 
 @pytest.fixture
@@ -11,14 +11,10 @@ def germline_segment():
             self.aligned_target = aligned_target
 
     class GermlineSegment:
-        def __init__(
-            self, realignment, germline_start, germline_end, query_start, query_end
-        ):
+        def __init__(self, realignment, germline_start, query_start):
             self.realignment = realignment
             self.germline_start = germline_start
-            self.germline_end = germline_end
             self.query_start = query_start
-            self.query_end = query_end
 
     realignment = Realignment(
         aligned_query="ATCG",
@@ -27,9 +23,7 @@ def germline_segment():
     germline_segment = GermlineSegment(
         realignment=realignment,
         germline_start=0,
-        germline_end=4,
         query_start=0,
-        query_end=4,
     )
     return germline_segment
 
@@ -41,25 +35,58 @@ def germline_segment():
 
 def test_make_cigar(germline_segment):
     # test make_cigar function
-    cigar = make_cigar(germline_segment)
-    assert cigar == "4M"
+    cigar = make_cigar(
+        query_start=germline_segment.query_start,
+        germline_start=germline_segment.germline_start,
+        aligned_query=germline_segment.realignment.aligned_query,
+        aligned_germline=germline_segment.realignment.aligned_target,
+    )
+    assert cigar == "4="
 
 
-def test_make_alignment_cigar():
+def test_allmatch_make_alignment_cigar():
     # test make_alignment_cigar function with identical sequences
     query = "ATCG"
     target = "ATCG"
     cigar = make_alignment_cigar(query, target)
-    assert cigar == "4M"
+    assert cigar == "4="
 
+
+def test_mismatch_make_alignment_cigar():
     # test make_alignment_cigar function with different sequences
     query = "ATCG"
     target = "AGCG"
     cigar = make_alignment_cigar(query, target)
-    assert cigar == "1M1D2M"
+    assert cigar == "1=1X2="
 
+
+def test_insertion_make_alignment_cigar():
     # test make_alignment_cigar function with insertions and deletions
     query = "ATCG"
-    target = "A-TG"
+    target = "A-CG"
     cigar = make_alignment_cigar(query, target)
-    assert cigar == "1M1D1M1I1M"
+    assert cigar == "1=1I2="
+
+
+def test_insertion_mismatch_make_alignment_cigar():
+    # test make_alignment_cigar function with insertions and deletions
+    query = "ATGG"
+    target = "A-CG"
+    cigar = make_alignment_cigar(query, target)
+    assert cigar == "1=1I1X1="
+
+
+def test_deletion_make_alignment_cigar():
+    # test make_alignment_cigar function with insertions and deletions
+    query = "A-CG"
+    target = "ATCG"
+    cigar = make_alignment_cigar(query, target)
+    assert cigar == "1=1D2="
+
+
+def test_deletion_mismatch_make_alignment_cigar():
+    # test make_alignment_cigar function with insertions and deletions
+    query = "A-GG"
+    target = "ATCG"
+    cigar = make_alignment_cigar(query, target)
+    assert cigar == "1=1D1X1="
