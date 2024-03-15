84c36fc67a059aa7844802a862f3e1336dd796ad
Bryan Briney <bryanbriney@gmail.com>
2024-03-15 12:05:04 -0700

    isotype tests

--- abstar/tests/test_isotype.py
+++ abstar/tests/test_isotype.py
@@ -1,29 +1,32 @@
 #!usr/env/python
 # filename: test_isotype.py
 
+import csv
 import os
-import sys
 
-from Bio import SeqIO
-
-from abutils.core.sequence import Sequence
-
-from .mock_classes import MockAntibody
-from ..core import abstar
-from ..core.antibody import Antibody
 from ..utils.isotype import get_isotype
 
 
-def test_isotype_bcr():
-    test_fasta = os.path.abspath('abstar/test_data/test_isotype.fasta')
-    with open(test_fasta, 'r') as f:
-        test_seqs = [Sequence(s) for s in SeqIO.parse(f, 'fasta')]
-    vdj_nt = [s for s in test_seqs if s.id == 'vdj_nt'][0].sequence
-    oriented_input = [s for s in test_seqs if s.id == 'oriented_input'][0].sequence
-    antibody = MockAntibody()
-    antibody.species = 'human'
-    antibody.germ_db = 'human'
-    antibody.vdj_nt = vdj_nt
-    antibody.oriented_input = oriented_input
-    isotype = get_isotype(antibody)
-    assert isotype.isotype == 'IgG1'
+class Antibody:
+    def __init__(
+        self,
+        isotype: str,
+        vdj_nt: str,
+        oriented_input: str,
+        species: str = "human",
+        germ_db: str = "human",
+    ):
+        self.isotype = isotype
+        self.vdj_nt = vdj_nt
+        self.oriented_input = oriented_input
+        self.species = species
+        self.germ_db = germ_db
+
+
+def test_get_isotype():
+    with open(os.path.abspath("abstar/test_data/test_get_isotype.csv"), "r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            antibody = Antibody(**row)
+            isotype = get_isotype(antibody)
+            assert isotype.isotype == antibody.isotype
