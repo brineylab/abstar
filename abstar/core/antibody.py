6ca8dabe8215cb36a2f0f804607696b3b4f80cad
Bryan Briney <bryanbriney@gmail.com>
2024-06-25 19:05:21 -0700

    start rewrite

--- abstar/core/antibody.py
+++ abstar/core/antibody.py
@@ -22,6 +22,16 @@
 #
 
 
+class Antibody:
+    """
+    docstring for Antibody
+    """
+
+    # TODO:
+
+    pass
+
+
 # from __future__ import absolute_import, division, print_function, unicode_literals
 
 import traceback
