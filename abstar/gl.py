5275e6c16cb9539cb76e94d6d1083186da4225a0
Bryan Briney <bryanbriney@gmail.com>
2024-07-11 23:50:46 -0700

    Update gl.py

--- abstar/gl.py
+++ abstar/gl.py
@@ -7,11 +7,11 @@ from .annotation.germline import *
 
 #  TODO:
 #
-#  should think about the pros/cons of building a single germline database per species that
+#  should think about the pros/cons of building a single, "combined" germline database per species that
 #  contains both BCR and TCR germline segments
 #
-#  the biggest con is the fact that OGRDB only has BCR data, so all databases would need to be
-#  hybrid of IMGT and OGRDB datasets -- maybe this just needs some more time??
+#  the most obvious con is the fact that OGRDB only has BCR data, so combined databases would need to be
+#  mixture of IMGT and OGRDB datasets -- maybe this just needs some more time and OGRDB will eventually get TCR data?
 #
 #  functions:
 #    - build_database() -- has `schema` kwarg, which can be either "ogrdb" or "imgt"
