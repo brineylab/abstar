c7aeed0164d48b990cac35b3ba3d77cf8ea81ba9
Bryan Briney <bryanbriney@gmail.com>
2024-08-06 00:03:35 -0700

    docs

--- abstar/utils.py
+++ abstar/utils.py
@@ -2,10 +2,35 @@
 # Distributed under the terms of the MIT License.
 # SPDX-License-Identifier: MIT
 
+from typing import Optional
+
 import click
 
 
-def parse_dict_from_string(ctx, param, value):
+def parse_dict_from_string(
+    ctx: click.Context, param: click.Parameter, value: Optional[str]
+) -> dict:
+    """
+    Parse a string of key-value pairs into a dictionary. Used as a ``click`` callback function
+    for parsing command-line arguments.
+
+    Parameters
+    ----------
+    ctx : click.Context
+        The click context.
+
+    param : click.Parameter
+        The click parameter.
+
+    value : Optional[str]
+        The value to parse. If ``None``, return an empty dictionary.
+
+    Returns
+    -------
+    dict
+        The parsed dictionary.
+
+    """
     if not value:
         return {}
     try:
