6df972c7a254f02cfd266fa0531cc052ac3e57f8
Bryan Briney <bryanbriney@gmail.com>
2024-08-06 00:00:31 -0700

    reconfigure top-level imports

--- abstar/__init__.py
+++ abstar/__init__.py
@@ -5,6 +5,5 @@ from Bio import BiopythonWarning
 warnings.simplefilter("ignore", BiopythonWarning)
 warnings.simplefilter(action="ignore", category=FutureWarning)
 
-from .core.abstar import create_parser, main, run, run_standalone, validate_args
-from .utils.preprocess import adapter_trim, fastqc, quality_trim
+from .core.abstar import run
 from .version import __version__
